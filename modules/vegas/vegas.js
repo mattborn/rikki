c8f054ca256f994642a3eee8ffc4439da30eb629
Matthew Born <me+code@mattborn.com>
2013-08-13 18:01:20 -0500

    Add form behavior, validation + error handling, tweak a few other things

--- modules/vegas/vegas.js
+++ modules/vegas/vegas.js
@@ -1,13 +1,78 @@
 (function ($) {
 
+	function validate() {
+		var bool = $('.vegas-bool-option.selected').data('value'),
+			going = bool === 1 || 0;
+		if (!$('.vegas-bool-option').hasClass('selected')) {
+			$('.vegas-bool .error').show();
+		}
+		if ($('.vegas-rsvp-name').val() === '') {
+			$('.vegas-form .error:eq(0)').show();
+		}
+		if ($('.vegas-rsvp-email').val() === '') {
+			$('.vegas-form .error:eq(1)').show();
+		}
+		if (going && !$('.vegas-hotel').hasClass('selected')) {
+			$('.vegas-hotels .error').show();
+		}
+		if ($('.error').is(':visible')) {
+			var top = $('.error:visible:eq(0)').offset().top - 30;
+			$('body, html').animate({scrollTop: top}, 300);
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	$('.vegas-bool-option').click(function () {
+		var error = $(this).parent().find('.error');
 		$('.vegas-bool-option').removeClass('selected');
 		$(this).addClass('selected');
+		if ($(this).data('value')) {
+			$('.vegas-toggle:hidden').show();
+		} else {
+			$('.vegas-toggle:visible').hide();
+		}
+		if (error.is(':visible')) {
+			error.slideUp();
+		}
+	});
+
+	$('.vegas-rsvp-input').blur(function () {
+		var error = $(this).prev('.error');
+		if (error.is(':visible')) {
+			error.slideUp();
+		}
 	});
 
 	$('.vegas-hotel').click(function () {
+		var error = $(this).parent().find('.error');
 		$('.vegas-hotel').removeClass('selected');
 		$(this).addClass('selected');
+		if (error.is(':visible')) {
+			error.slideUp();
+		}
+	});
+
+	$('[href^="//"]').on('click', function (e) {
+		window.open($(this).attr('href'));
+		return false;
+	});
+
+	$('.vegas-submit-button').click(function () {
+		var rsvp = {
+			going: $('.vegas-bool-option.selected').data('value'),
+			name: $('.vegas-rsvp-name').val(),
+			email: $('.vegas-rsvp-email').val(),
+			plus: $('.vegas-rsvp-plus').val(),
+			hotel: null
+		}
+		if (rsvp.going) {
+			rsvp.hotel = $('.selected .vegas-hotel-name').html();
+		}
+		if (validate()) {
+			console.log(rsvp);
+		}
 	});
 
 })(jQuery);
--- modules/vegas/vegas.php
+++ modules/vegas/vegas.php
@@ -1,12 +1,12 @@
 
-<header class="vegas-header">
+<header class="vegas-header" role="banner">
 	<div class="vegas-header-center">
 		<h1 class="vegas-title">Rikki is turning 21.</h1>
 		<h2 class="vegas-subtitle">We are celebrating in Las Vegas.</h2>
 	</div>
 </header>
 
-<div class="vegas-rsvp">
+<div class="vegas-rsvp" role="main">
 	<section class="vegas-section">
 		<h1 class="vegas-rsvp-h1">RSVP</h1>
 	</section>
@@ -17,6 +17,7 @@
 	</section>
 
 	<section class="vegas-section vegas-bool">
+		<p class="error">You gotta tell us if you are coming or not!</p>
 		<a class="vegas-bool-option" data-value="1">Yes :D</a>
 		<a class="vegas-bool-option" data-value="0">No :(</a>
 	</section>
@@ -27,19 +28,23 @@
 	</section>
 
 	<section class="vegas-section vegas-form">
+		<p class="error">Did you forget your name?</p>
 		<input class="vegas-rsvp-input vegas-rsvp-name" placeholder="Full Name">
+		<p class="error">You must enter a valid email address.</p>
 		<input class="vegas-rsvp-input vegas-rsvp-email" placeholder="Email Address">
-
-		<label class="vegas-rsvp-label">How many are coming with you?</label>
-		<input class="vegas-rsvp-input vegas-rsvp-plus" placeholder="+0">
+		<div class="vegas-toggle">
+			<label class="vegas-rsvp-label">How many are coming with you?</label>
+			<input class="vegas-rsvp-input vegas-rsvp-plus" placeholder="+0">
+		</div>
 	</section>
 
-	<section class="vegas-section vegas-step-3">
+	<section class="vegas-section vegas-step-3 vegas-toggle">
 		<h2 class="vegas-rsvp-h2">Step 3</h2>
 		<p class="vegas-rsvp-p">Pick a hotel and we’ll try to get a group rate.</p>
 	</section>
 
-	<section class="vegas-section vegas-hotels">
+	<section class="vegas-section vegas-hotels vegas-toggle">
+		<p class="error">Don’t forget to pick a hotel!</p>
 		<div class="vegas-hotel">
 			<h3 class="vegas-hotel-name">Aria</h3>
 			<p class="vegas-hotel-blurb">The most technologically advanced hotel ever built.</p>
@@ -53,12 +58,12 @@
 			<p class="vegas-hotel-blurb">The oldest resort on the Strip.</p>
 		</div>
 		<div class="vegas-hotel">
-			<h3 class="vegas-hotel-name">Harrahs</h3>
+			<h3 class="vegas-hotel-name">Harrah’s</h3>
 			<p class="vegas-hotel-blurb">The biggest casino brand in America.</p>
 		</div>
 		<div class="vegas-hotel">
 			<h3 class="vegas-hotel-name">Planet Hollywood</h3>
-			<p class="vegas-hotel-blurb">Where everybody's famous.</p>
+			<p class="vegas-hotel-blurb">Where everybody’s famous.</p>
 		</div>
 	</section>
 	
